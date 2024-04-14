3a5e9269542b751a29e2ebb41433be09a09275e1
alex <sytnikov.zp@gmail.com>
2024-04-14 22:40:45 +0300

    Add getDerivedStateFromProps

--- src/components/WatchList/WatchList.jsx
+++ src/components/WatchList/WatchList.jsx
@@ -2,7 +2,21 @@ import { Component } from 'react';
 import WatchItem from '../WatchItem/WatchItem';
 
 export class WatchList extends Component {
+  state = {
+    count: 0,
+  };
+
+  static getDerivedStateFromProps(props, state) {
+    if (props.movies.length !== state.count) {
+      return {
+        count: props.movies.length,
+      };
+    }
+  }
+
   render() {
+    console.log(this.state.count);
+    console.log(this.props.movies);
     return (
       <>
         {this.props.movies.map((movie) => {
