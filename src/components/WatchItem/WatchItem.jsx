d1118bbe93b3ad46d12342b806b96555606b6476
alex <sytnikov.zp@gmail.com>
2024-04-16 20:17:18 +0300

    Refactoring WatchItem to functional component

--- src/components/WatchItem/WatchItem.jsx
+++ src/components/WatchItem/WatchItem.jsx
@@ -1,29 +1,38 @@
-import { Component } from 'react';
 import './WatchItem.css';
 
-export class WatchItem extends Component {
-  onMovieDelete = (event) => {
+const divStyles = {
+  color: 'white',
+};
+
+function toggleBackground(movie) {
+  return {
+    ...divStyles,
+    backgroundColor: movie.isDone ? 'cadetblue' : 'darkgoldenrod',
+  };
+}
+
+export const WatchItem = ({ movie, onToggle, onDelete }) => {
+  const onMovieDelete = (event) => {
     event.stopPropagation();
-    this.props.onDelete(this.props.movie.id);
+    onDelete(id);
   };
 
-  render() {
-    const { id, title, director, isDone } = this.props.movie;
-    const onToggle = this.props.onToggle;
-    return (
-      <div
-        className={'watch-item ' + (isDone ? 'done' : '')}
-        onClick={() => onToggle(id)}
-      >
-        <p className='content'>
-          {title} directed by {director}
-        </p>
-        <span className='delete-btn' onClick={this.onMovieDelete}>
-          X
-        </span>
-      </div>
-    );
-  }
-}
+  const { id, title, director } = movie;
+
+  return (
+    <div
+      className='watch-item'
+      style={toggleBackground(movie)}
+      onClick={() => onToggle(id)}
+    >
+      <p className='content'>
+        {title} directed by {director}
+      </p>
+      <span className='delete-btn' onClick={onMovieDelete}>
+        X
+      </span>
+    </div>
+  );
+};
 
 export default WatchItem;
