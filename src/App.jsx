cfe9c173b3e7787b8b0e25c42775111706a1382f
alex <sytnikov.zp@gmail.com>
2024-04-23 20:04:31 +0300

    Implement saving to server

--- src/App.jsx
+++ src/App.jsx
@@ -2,49 +2,47 @@ import { useEffect, useState } from 'react';
 import './App.css';
 import WatchList from './components/WatchList/WatchList';
 import WatchForm from './components/WatchForm/WatchForm';
-import { customAlphabet } from 'nanoid';
+import api from './api/movie-service';
 
 function App() {
   const [arrMovies, setArrMovies] = useState([]);
 
-  useEffect(getFromStorage, []);
-
-  function getFromStorage() {
-    const movies = JSON.parse(localStorage.getItem('movies'));
-    if (!movies) {
-      setArrMovies([]);
-    } else {
-      setArrMovies(movies);
-    }
-  }
+  useEffect(() => {
+    api.get('/').then(({ data }) => {
+      if (!data) {
+        setArrMovies([]);
+      } else {
+        setArrMovies(data);
+      }
+    });
+  }, []);
 
   function toggleToWatch(id) {
-    const movies = arrMovies.map((movie) => {
-      if (movie.id !== id) {
-        return movie;
-      }
-      return { ...movie, isDone: !movie.isDone };
-    });
-    setArrMovies(movies);
-    saveMovies(movies);
+    const updatedMovie = arrMovies.find((movie) => movie.id === id);
+    updatedMovie.isDone = !updatedMovie.isDone;
+    api
+      .put(`/${id}`, updatedMovie)
+      .then(({ data }) => {
+        setArrMovies(
+          arrMovies.map((movie) => {
+            return movie.id !== id ? movie : data;
+          })
+        );
+      })
+      .catch((error) => console.log(error));
   }
 
   const addMovie = (movie) => {
-    const nanoid = customAlphabet('1234567890', 3);
-    movie.id = nanoid();
-    const movies = [...arrMovies, movie];
-    setArrMovies(movies);
-    saveMovies(movies);
+    api.post('/', movie).then(({ data }) => {
+      const newMovies = [...arrMovies, data];
+      setArrMovies(newMovies);
+    });
   };
 
   const deleteMovie = (id) => {
-    const movies = arrMovies.filter((movie) => movie.id !== id);
-    setArrMovies(movies);
-    saveMovies(movies);
-  };
-
-  const saveMovies = (movies) => {
-    localStorage.setItem('movies', JSON.stringify(movies));
+    api.delete(`/${id}`).then(({ status }) => console.log(status));
+    const newMovies = arrMovies.filter((movie) => movie.id !== id);
+    setArrMovies(newMovies);
   };
 
   return (
