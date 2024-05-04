1b228009ab1bd9a24d5563ad6b3de7a95226779d
alex <sytnikov.zp@gmail.com>
2024-05-04 13:35:46 +0300

    Edit moviesReducer

--- src/store/reducers/moviesReducer.js
+++ src/store/reducers/moviesReducer.js
@@ -3,34 +3,68 @@ import ACTION_TYPES from '../actions/actionTypes';
 
 const initialState = {
   movies: moviesState,
+  isFetching: false,
+  error: null,
 };
 
 export default function moviesReducer(state = initialState, { type, payload }) {
   switch (type) {
-    case ACTION_TYPES.ADD_MOVIE:
+    // Success
+    case ACTION_TYPES.POST_MOVIE_SUCCESS:
       return {
         ...state,
         movies: [...state.movies, payload],
+        isFetching: false,
       };
-    case ACTION_TYPES.DELETE_MOVIE:
+
+    case ACTION_TYPES.DELETE_MOVIE_SUCCESS:
       return {
         ...state,
         movies: [...state.movies.filter((movie) => movie.id !== payload)],
+        isFetching: false,
       };
-    case ACTION_TYPES.TOGGLE_MOVIE:
+
+    case ACTION_TYPES.PUT_MOVIE_SUCCESS:
       return {
         ...state,
         movies: [
           ...state.movies.map((movie) =>
-            movie.id === payload ? { ...movie, isDone: !movie.isDone } : movie
+            movie.id === payload.id
+              ? { ...movie, isDone: !movie.isDone }
+              : movie
           ),
         ],
+        isFetching: false,
       };
-    case ACTION_TYPES.GET_MOVIES:
+
+    case ACTION_TYPES.GET_MOVIES_SUCCESS:
       return {
         ...state,
         movies: payload,
+        isFetching: false,
       };
+
+    // Requesting
+    case ACTION_TYPES.POST_MOVIE_REQUEST:
+    case ACTION_TYPES.DELETE_MOVIE_REQUEST:
+    case ACTION_TYPES.PUT_MOVIE_REQUEST:
+    case ACTION_TYPES.GET_MOVIES_REQUEST:
+      return {
+        ...state,
+        isFetching: true,
+      };
+
+    // Error
+    case ACTION_TYPES.POST_MOVIE_ERROR:
+    case ACTION_TYPES.DELETE_MOVIE_ERROR:
+    case ACTION_TYPES.PUT_MOVIE_ERROR:
+    case ACTION_TYPES.GET_MOVIES_ERROR:
+      return {
+        ...state,
+        isFetching: false,
+        error: payload,
+      };
+
     default:
       return state;
   }
